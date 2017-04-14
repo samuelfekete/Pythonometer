5c0c3a937b13e57219bc5158fa81a2628fb81246
Samuel Fekete <samuel.fekete@hogarthww.com>
2017-04-14 19:44:42 +0100

    Add more exception questions

--- pythonometer/questions/standard_library/built_in_exceptions.py
+++ pythonometer/questions/standard_library/built_in_exceptions.py
@@ -1,4 +1,4 @@
-"""Questions for the built-in exceptions.
+"""Questions about the built-in exceptions.
 
 Docs: https://docs.python.org/3.6/library/exceptions.html
 """
@@ -6,32 +6,77 @@ Docs: https://docs.python.org/3.6/library/exceptions.html
 from ..base import Question
 
 
-class CauseAssertionError(Question):
+class CauseExceptionMixin(object):
+    """A mixin for questions about causing exceptions.
+
+    Implements functionality that is common among exception-causing questions.
+    """
+
+    def __init__(self):
+        self.exception = Exception
+
+    def get_question_text(self):
+        return (
+            'Cause an {exception_name}.\n\n'
+            'Write some code that will raise an {exception_name}.\n\n'
+            'Your code may not contain the words `raise` or `exec`.'
+        ).format(exception_name=self.exception.__name__)
+
+    def check_answer(self, answer):
+        for illegal_word in ['raise', 'exec']:
+            # `exec` is banned because it can be used to construct `raise`.
+            if illegal_word in answer:
+                return False
+        try:
+            exec(answer, {}, {})
+        except self.exception:
+            return True
+        except:
+            return False
+
+    def get_wrong_answers(self):
+        return ['raise {}'.format(self.exception.__name__)]
+
+
+class CauseAssertionError(CauseExceptionMixin, Question):
     """Cause an AssertionError.
 
     https://docs.python.org/3.6/library/exceptions.html#AssertionError
     """
 
-    def get_question_text(self):
-        return (
-            'Cause an AssertionError.\n\n'
-            'Write some code that will raise an AssertionError.\n\n'
-            'Your code may not contain the words `raise` or `exec`.'
-        )
-
-    def check_answer(self, answer):
-        for illegal_word in ['raise', 'exec']:
-            if illegal_word in answer:
-                return False
-        try:
-            exec(answer, {}, {})
-        except AssertionError:
-            return True
-        except:
-            return False
+    def __init__(self):
+        self.exception = AssertionError
 
     def get_correct_answers(self):
         return ['assert(False)']
 
-    def get_wrong_answers(self):
-        return ['raise AssertionError']
+
+class CauseAttributeError(CauseExceptionMixin, Question):
+    """Cause an AttributeError.
+
+    https://docs.python.org/3.6/library/exceptions.html#AttributeError
+    """
+
+    def __init__(self):
+        self.exception = AttributeError
+
+    def get_correct_answers(self):
+        return ['"".foo']
+
+
+class CauseEOFError(CauseExceptionMixin, Question):
+    """Cause an EOFError.
+
+    https://docs.python.org/3.6/library/exceptions.html#EOFError
+    """
+
+    def __init__(self):
+        self.exception = EOFError
+
+    def get_correct_answers(self):
+        return [(
+            'import StringIO\n'
+            'import sys\n'
+            'sys.stdin = StringIO.StringIO("")\n'
+            'input()\n'
+        )]
